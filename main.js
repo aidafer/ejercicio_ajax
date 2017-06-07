584d1944b652c9bd2e555fa25825ec4634b06714
aidafer <aidafercle@gmail.com>
2017-06-07 20:27:00 +0200

    estilos sass y javascript correcto

--- main.js
+++ main.js
@@ -12,24 +12,22 @@ var request = new XMLHttpRequest();
 
 btn.addEventListener("click", requestInfo);
 
-request.onload = requestInfo;
+
 function requestInfo() {
-  if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    request.open('GET', 'https://api.github.com/users/' +userId.value, true);
-    nameUser.innerHTML = data.login;
-    picture.src = data.avatar_url;
-    num.innerHTML = data.public_repos;
-  } else {
-    console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
-  }
-};
-
-
-request.onerror = function() {
-  console.log('Error al tratar de conectarse con el servidor');
-};
-
-
-//3-request.send: manda la petición al servidor
-request.send();
+  request.open('GET', 'https://api.github.com/users/' +userId.value, true);
+  // request.onload();
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      nameUser.innerHTML = data.login;
+      picture.setAttribute('src', data.avatar_url);
+      num.innerHTML = data.public_repos;
+    } else {
+      console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
+    }
+  };
+  request.onerror = function() {
+    console.log('Error al tratar de conectarse con el servidor');
+  };
+  request.send();
+}
